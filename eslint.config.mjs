af6c608d881d0746129ee100b4b68f57e0aaad45
Vinicius Gabriel <viiniciusgs03@gmail.com>
2024-12-27 18:10:28 -0300

    style: format code with prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
 import eslintConfigPrettier from 'eslint-config-prettier'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
