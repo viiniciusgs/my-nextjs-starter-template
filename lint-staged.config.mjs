eb941237390ac02a43d5f74769945edd76fac605
Vinicius Gabriel <viiniciusgs03@gmail.com>
2025-01-13 15:05:27 -0300

    chore: add script to format files with prettier

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -3,7 +3,7 @@ import path from 'path'
 const config = {
   /* This will format all files, excluding JavaScript, TypeScript,
   files in the .husky folder, and the pnpm-lock.yaml file */
-  '!(*.js|*.ts|*.cjs|*.mjs|*.d.cts|*.d.mts|*.jsx|*.tsx|.husky/**|pnpm-lock.yaml)':
+  '!(*.js|*.ts|*.cjs|*.mjs|*.d.cts|*.d.mts|*.jsx|*.tsx|.husky/**|.prettierignore)':
     (filenames) =>
       `prettier --write ${filenames
         .map((f) => path.relative(process.cwd(), f))
